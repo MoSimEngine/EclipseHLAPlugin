6c574605207aea5ded80fc5d8a1b97a718036505
Johannes Grün <gruen.jojo.develop@gmail.com>
2018-01-04 13:26:39 +0100

    Hinzufügen der Dateien

---
+++ plugins/edu.kit.ipd.sdq.modsim.hla.example/examples/edu.kit.ipd.sdq.modsim.hla.example.restaurant/src/hla/test/Main.java
@@ -0,0 +1,63 @@
+package hla.test;
+
+import java.io.File;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+public class Main {
+	// public static void main(String[] args) {
+	//
+	// Federate federate = new EMFModelLoad().load();
+	//
+	// federate.setHLAFederateAmbassador(new NullFederateAmbassador());
+	//
+	// System.out.println("Out");
+	// }
+
+	private static void transform() throws TransformerException {
+		TransformerFactory factory = TransformerFactory.newInstance();
+		Source xslt = new StreamSource(new File("model/transform_omt_to_fomxml.xslt"));
+		Transformer transformer = factory.newTransformer(xslt);
+
+		Source text = new StreamSource(new File("model/RestaurantDrinks.omt"));
+		transformer.transform(text, new StreamResult(new File("model/tmp/RestaurantDrinks.xml")));
+		
+		text = new StreamSource(new File("model/RestaurantFood.omt"));
+		transformer.transform(text, new StreamResult(new File("model/tmp/RestaurantFood.xml")));
+				
+		text = new StreamSource(new File("model/RestaurantProcesses.omt"));
+		transformer.transform(text, new StreamResult(new File("model/tmp/RestaurantProcesses.xml")));
+		
+		text = new StreamSource(new File("model/RestaurantSoup.omt"));
+		transformer.transform(text, new StreamResult(new File("model/tmp/RestaurantSoup.xml")));
+	}
+
+	public static void main(String[] args) {
+
+		try {
+			transform();
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// get a federate name, use "exampleFederate" as default
+		String federateName = "exampleFederate";
+		if (args.length != 0) {
+			federateName = args[0];
+		}
+
+		try {
+			// run the example federate
+			new ExampleFederate().runFederate(federateName);
+		} catch (Exception rtie) {
+			// an exception occurred, just log the information and exit
+			rtie.printStackTrace();
+		}
+	}
+}
