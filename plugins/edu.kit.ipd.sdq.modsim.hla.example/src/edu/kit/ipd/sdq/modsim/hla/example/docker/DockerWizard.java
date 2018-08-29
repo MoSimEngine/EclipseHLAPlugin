026b5bd85d6136eeabbf943ac4c6553ad27fcbbb
Florian Böhle <florian.boehle@student.kit.edu>
2018-08-29 21:02:56 +0200

    Added linux install script.

--- plugins/edu.kit.ipd.sdq.modsim.hla.example/src/edu/kit/ipd/sdq/modsim/hla/example/docker/DockerWizard.java
+++ plugins/edu.kit.ipd.sdq.modsim.hla.example/src/edu/kit/ipd/sdq/modsim/hla/example/docker/DockerWizard.java
@@ -38,6 +38,15 @@ public class DockerWizard extends ExampleInstallerWizard {
 		protected MessageBox linuxProceedMessageBox;
 		private String hostOS = System.getProperty("os.name").toLowerCase();
 		private String dockerButtonText = "Docker Portico container RTI engine";
+		private String linuxInstallCommand = "sudo apt-get remove docker docker-engine docker-ce docker.io;"
+				+ "sudo apt-get update;"
+				+ "sudo apt-get install apt-transport-https ca-certificates curl software-properties-common;"
+				+ "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;"
+				+ "sudo apt-key fingerprint 0EBFCD88;"
+				+ "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\";"
+				+ "sudo apt-get update;"
+				+ "sudo apt-get install docker-ce;"
+				+ "sudo docker run hello-world;";
 		
 
 		public DockerProjectPage(String pageName, String title, ImageDescriptor titleImage) {
