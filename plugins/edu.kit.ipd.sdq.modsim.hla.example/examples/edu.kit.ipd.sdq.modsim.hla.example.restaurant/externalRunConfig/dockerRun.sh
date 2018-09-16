1d71898875a8ed26e4e9e3ec7ef516d32bbf3238
Florian Böhle <florian.boehle@student.kit.edu>
2018-09-16 22:31:21 +0200

    Beautified Launch Configs and Docker Run Scripts.

--- plugins/edu.kit.ipd.sdq.modsim.hla.example/examples/edu.kit.ipd.sdq.modsim.hla.example.restaurant/externalRunConfig/dockerRun.sh
+++ plugins/edu.kit.ipd.sdq.modsim.hla.example/examples/edu.kit.ipd.sdq.modsim.hla.example.restaurant/externalRunConfig/dockerRun.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -l
-#cd hla/
+# Start up docker container and get its ID
 containerID="$(docker run -d -i fboehle/portico:2.0.2 /bin/bash)"
+# Copy Java files and HLA model into container
 cd src
 docker cp ./. "${containerID}":/portico/hla/
 docker cp ../model/. "${containerID}":/portico/hla/model/
-#docker attach "${containerID}"
+# Compile and execute Portico project with OpenJDK and Portico library from docker container
 docker exec -i "${containerID}" sh -c "cd hla && javac -cp /portico/lib/portico.jar hla/test/*.java && jar -cf java-hla.jar hla/test/*.class && java -cp ./java-hla.jar:/portico/lib/portico.jar hla.test.Main"
-#docker exec -i "${containerID}" sh -c "cd hla && javac -cp /portico/lib/portico.jar hla/test/*.java"
-#ls -al
